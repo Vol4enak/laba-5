45c0622c3863a978c8d039d473b5bf9c54443113
Vol4enak <52600927+Vol4enak@users.noreply.github.com>
2023-05-18 19:45:02 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
+import { carsItems } from '../data/cars';
+import BMW from './img/Audi-A8-Edition-21.jpg';
+import Audi from './img/BMW-M2-Coupe.jpg';
+import Bentley from './img/bentley-continental-gt-speed-black-edition.jpg';
+import Mercedes from './img/mercedes-benz-coupe.jpg';
 export const App = () => {
+  const cars = [
+    { name: 'BMW M2 Coupe', price: '40000', img: BMW },
+    { name: 'Audi-A8-Edition ', price: '39000', img: Audi },
+    { name: 'bentley-continental-gt ', price: '52000', img: Bentley },
+    {
+      name: 'mercedes-benz-coupe',
+      price: '42000',
+      img: Mercedes,
+    },
+  ];
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      {cars.map(({ name, price, img }) => {
+        return (
+          <div className="card" key={name}>
+            <div className="card-img">
+              <img src={img} alt={name} />
+            </div>
+            <h3>{name}</h3>
+            <p>{price}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
---
+++ src/data/cars.js
@@ -0,0 +1,22 @@
+export const carsItems = [
+  {
+    name: 'BMW M2 Coupe',
+    price: '40000',
+    img: 'BMW-M2-Coupe.jpg',
+  },
+  {
+    name: 'Audi-A8-Edition ',
+    price: '39000',
+    img: 'Audi-A8-Edition-21.jpg',
+  },
+  {
+    name: 'bentley-continental-gt ',
+    price: '52000',
+    img: 'bentley.jpg',
+  },
+  {
+    name: 'mercedes-benz-coupe',
+    price: '42000',
+    img: 'mercedes-benz-coupe.jpg',
+  },
+];
